786b851a1a10264168ec6728b83d6a0e4604b2c7
Gpooja13 <145600866+Gpooja13@users.noreply.github.com>
2024-10-07 04:58:52 +0530

    second commit

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Background from "../components/Background";
 import Header from "../components/Header";
+import {firebaseAuth} from "../utils/firebase-config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth/cordova";
+import { useNavigate } from "react-router-dom";
+
 
 const Signup = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate=useNavigate();
+
+  const handleSignIn = async () => {
+    try {
+    const {email,password}=formValues;
+    await createUserWithEmailAndPassword(firebaseAuth,email,password);
+  } catch (error) {
+      console.log(error);
+    }
+  };
+
+  onAuthStateChanged(firebaseAuth,(currentUser)=>{
+    if(currentUser) navigate("/");
+  })
+
   return (
-    <Container>
+    <Container showPassword={showPassword}>
       <Background />
       <div className="content">
-        <Header />
+        <Header login />
         <div className="body flex column a-center j-center">
           <div className="text flex column">
             <h1>Unlimited movies, TV shows and more</h1>
@@ -18,11 +43,37 @@ const Signup = () => {
             </h6>
           </div>
           <div className="form">
-            <input type="email" placeholder="Email Address" name="email" />
-            <input type="password" placeholder="Password" name="password" />
-            <button>Get Started</button>
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={formValues.email}
+              onChange={(e) =>
+                setFormValues({
+                  ...formValues,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            />
+            {showPassword && (
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={formValues.password}
+                onChange={(e) =>
+                  setFormValues({
+                    ...formValues,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+              />
+            )}
+            {!showPassword && (
+              <button onClick={() => setShowPassword(true)}>Get Started</button>
+            )}
           </div>
-          <button>Log In</button>
+          <button onClick={() => handleSignIn()}>Sign Up</button>
         </div>
       </div>
     </Container>
@@ -32,7 +83,7 @@ const Signup = () => {
 const Container = styled.div`
   position: relative;
   .content {
-    position:absolute;
+    position: absolute;
     top: 0;
     left: 0;
     background-color: rgba(0, 0, 0, 0.5);
@@ -40,6 +91,49 @@ const Container = styled.div`
     width: 100vw;
     display: grid;
     grid-template-rows: 15vh 85vh;
+    .body {
+      gap: 1rem;
+      text-align: center;
+      font-size: 2rem;
+      h1 {
+        padding: 0.25rem;
+      }
+    }
+    .form {
+      display: grid;
+      grid-template-columns: ${({ showPassword }) =>
+        showPassword ? "1fr 1fr" : "2fr 1fr"};
+      width: 60%;
+      input {
+        color: black;
+        border: none;
+        padding: 1.5rem;
+        font-size: 1.2rem;
+        border: 1px solid black;
+        &:focus {
+          outline: none;
+        }
+      }
+      button {
+        padding: 0.5rem 1rem;
+        background-color: #e50914;
+        border: none;
+        cursor: pointer;
+        color: white;
+        font-weight: bolder;
+        font-size: 1.05rem;
+      }
+    }
+    button {
+      padding: 0.5rem 1rem;
+      background-color: #e50914;
+      border: none;
+      border-radius: 0.2rem;
+      cursor: pointer;
+      color: white;
+      font-weight: bolder;
+      font-size: 1.05rem;
+    }
   }
 `;
 export default Signup;
